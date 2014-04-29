dffd26279768a398a3406166c310c28f550c5991
gaotong2055 <gaotong2055@gmail.com>
2014-04-29 14:57:50 +0800

    first

---
+++ JavaSource/src/com/collection/LRUCache.java
@@ -0,0 +1,20 @@
+package com.collection;
+
+
+import java.util.LinkedHashMap;
+
+public class LRUCache {
+	LinkedHashMap<Integer, Integer> lmap;
+	  public LRUCache(int capacity) {
+	        
+	    }
+	    
+	    public int get(int key) {
+	    	
+	        return lmap.get(key);
+	    }
+	    
+	    public void set(int key, int value) {
+	        
+	    }
+}
